209ffb33035d41eea6fc3de9f971673a6f8fa7a1
John <adeniyijohn2002@gmail.com>
2025-04-19 01:40:42 +0100

    Refactor project structure to rename 'exbox' to 'bills-manager' and update settings accordingly

--- bills-manager/asgi.py
+++ bills-manager/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for exbox project.
+ASGI config for bills-manager project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exbox.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bills-manager.settings')
 
 application = get_asgi_application()
